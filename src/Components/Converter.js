adb244fb0a896f5769b4b9d157c0698fa3081c43
ayman jarmoune <ayman.jarmoune@gmail.com>
2024-03-20 17:49:51 +0000

    project start and text to pdf

---
+++ src/Components/Converter.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { saveAs } from "file-saver";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
+const Converter = () => {
+  const [text, setText] = useState("");
+
+  const convert = () => {
+    const docDefinition = {
+      content: [
+        {
+          text: text,
+        },
+      ],
+    };
+
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    pdfMake.createPdf(docDefinition).download("converted.pdf");
+  };
+
+  return (
+    <div>
+      <textarea onChange={(e) => setText(e.target.value)}></textarea>
+      <button onClick={convert}>Convert to PDF</button>
+    </div>
+  );
+};
+
+export default Converter;
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Converter from "./Converter";
+
+
+function Home() {
+  return (
+    <div>
+      <Converter/>
+    </div>
+  );
+}
+
+export default Home;
